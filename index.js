785c8fd714648af3e87f8856f35c4b7df4b64cb0
Paul Swail <paul@winterwindsoftware.com>
2018-04-12 13:54:37 +0100

    app version

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { version } = require('./package.json');
 
 const PORT = 8080;
 
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello world\n');
+    res.send(`Hello world!\n App version ${version}`);
 });
 app.get('/health', (req, res) => {
     res.json({ healthy: true });
